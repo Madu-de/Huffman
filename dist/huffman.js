0d00031e487bae672798d7d2e3478d89d5f3f899
Madu <kontakt.madu@gmail.com>
2022-11-14 12:53:35 +0100

    Add tsconfig

--- dist/huffman.js
+++ dist/huffman.js
@@ -1,64 +1,54 @@
-/* 
-    By Madu 
+/*
+    By Madu
     On 13.11.2022
     GNU Affero General Public License v3.0
 */
-
 class HuffmanNode {
-    public name: string;
-    public count: number;
-    public connections: any[];
-    constructor(connections: any[]) {
+    constructor(connections) {
         this.connections = connections;
         this.name = connections[0][0] + connections[1][0];
         this.count = connections[0][1] + connections[1][1];
     }
 }
-
-const word: string = 'MISSISSIPPI';
+const word = 'MISSISSIPPI';
 let [code, tree] = huffman(word, true);
 console.log(code);
 console.log(tree);
-
-function huffman(word: string, withSpace?: boolean): (string | Map<string, string>)[] {
-    let huffmancode: string = word;
-
-    const charTable: Map<string, number> = getCharTable(word);
+function huffman(word, withSpace) {
+    let huffmancode = word;
+    const charTable = getCharTable(word);
     const charArray = [...charTable];
     let sortedCharArray = charArray.sort((a, b) => a[1] - b[1]);
-    let nodes: HuffmanNode[] = generateAllHuffmanNodes(sortedCharArray);
-
-    let charToBinary: Map<string, string> = getAllCharsBinaryCode(nodes, charTable, withSpace);
-
+    let nodes = generateAllHuffmanNodes(sortedCharArray);
+    let charToBinary = getAllCharsBinaryCode(nodes, charTable, withSpace);
     charToBinary.forEach((code, char) => {
         huffmancode = huffmancode.replaceAll(char, code);
     });
     return [huffmancode, charToBinary];
 }
-
-function generateAllHuffmanNodes(sortedCharArray: [string, number][]): HuffmanNode[] {
-    let nodes: HuffmanNode[] = [
+function generateAllHuffmanNodes(sortedCharArray) {
+    let nodes = [
         new HuffmanNode([sortedCharArray[0], sortedCharArray[1]])
     ];
     sortedCharArray.splice(0, 2);
-    let node: HuffmanNode = nodes[0];
+    let node = nodes[0];
     sortedCharArray.forEach(char => {
         node = new HuffmanNode([[node.name, node.count], char]);
         nodes.push(node);
     });
     return nodes;
 }
-
-function getAllCharsBinaryCode(nodes: HuffmanNode[], charTable: Map<string, number>, withSpace: boolean): Map<string, string> {
-    let charToBinary: Map<string, string> = new Map<string, string>();
+function getAllCharsBinaryCode(nodes, charTable, withSpace) {
+    let charToBinary = new Map();
     let reversedNodes = nodes.reverse();
     [...charTable].forEach(charCount => {
         const char = charCount[0];
-        let code: string = '';
+        let code = '';
         reversedNodes.forEach(node => {
             if (node.connections[0][0].includes(char)) {
                 code += '0';
-            } else if (node.connections[1][0].includes(char)) {
+            }
+            else if (node.connections[1][0].includes(char)) {
                 code += '1';
             }
         });
@@ -66,14 +56,13 @@ function getAllCharsBinaryCode(nodes: HuffmanNode[], charTable: Map<string, numb
     });
     return charToBinary;
 }
-
-function getCharTable(word: string): Map<string, number> {
-    let table: Map<string, number> = new Map<string, number>();
-
-    const chars: string[] = word.split('');
+function getCharTable(word) {
+    let table = new Map();
+    const chars = word.split('');
     chars.forEach(char => {
-        table.set(char, (table.get(char) ?? 0) + 1);
+        var _a;
+        table.set(char, ((_a = table.get(char)) !== null && _a !== void 0 ? _a : 0) + 1);
     });
-
     return table;
 }
+//# sourceMappingURL=huffman.js.map
